aed5f5cdd68f61d7826aa053410d0fc71370a785
Игорь Золотых <zolotykh907@gmail.com>
2024-05-24 15:58:19 +0700

    final 8

--- tasks8/main2.cu
+++ tasks8/main2.cu
@@ -53,13 +53,13 @@ public:
     }
 };
 
-void write_matrix(const double* matrix, int N, const std::string& filename) {
+void write_matrix(const double* curmatrix, int N, const std::string& filename) {
     std::ofstream outputFile(filename);
     int fieldWidth = 10;
 
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            outputFile << std::setw(fieldWidth) << std::fixed << std::setprecision(4) << matrix[i * N + j];
+            outputFile << std::setw(fieldWidth) << std::fixed << std::setprecision(4) << curmatrix[i * N + j];
         }
         outputFile << std::endl;
     }
@@ -77,42 +77,42 @@ void init_error(Data<double>& matr, int size){
     }
 }
 
-void init(Data<double>& matrix, int size) {
+void init(Data<double>& curmatrix, int size) {
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            matrix.arr[i * size + j] = 0;
+            curmatrix.arr[i * size + j] = 0;
         }
     }
-    matrix.arr[0] = 10.0;
-    matrix.arr[size - 1] = 20.0;
-    matrix.arr[(size - 1) * size + (size - 1)] = 30.0;
-    matrix.arr[(size - 1) * size] = 20.0;
+    curmatrix.arr[0] = 10.0;
+    curmatrix.arr[size - 1] = 20.0;
+    curmatrix.arr[(size - 1) * size + (size - 1)] = 30.0;
+    curmatrix.arr[(size - 1) * size] = 20.0;
     for (int i = 1; i < size - 1; ++i) {
-        matrix.arr[i * size + 0] = linearInterpolation(i, 0.0, matrix.arr[0], size - 1, matrix.arr[(size - 1) * size]);
+        curmatrix.arr[i * size + 0] = linearInterpolation(i, 0.0, curmatrix.arr[0], size - 1, curmatrix.arr[(size - 1) * size]);
     }
     for (int i = 1; i < size - 1; ++i) {
-        matrix.arr[0 * size + i] = linearInterpolation(i, 0.0, matrix.arr[0], size - 1, matrix.arr[size - 1]);
+        curmatrix.arr[0 * size + i] = linearInterpolation(i, 0.0, curmatrix.arr[0], size - 1, curmatrix.arr[size - 1]);
     }
     for (int i = 1; i < size - 1; ++i) {
-        matrix.arr[(size - 1) * size + i] = linearInterpolation(i, 0.0, matrix.arr[(size - 1) * size], size - 1, matrix.arr[(size - 1) * size + (size - 1)]);
+        curmatrix.arr[(size - 1) * size + i] = linearInterpolation(i, 0.0, curmatrix.arr[(size - 1) * size], size - 1, curmatrix.arr[(size - 1) * size + (size - 1)]);
     }
     for (int i = 1; i < size - 1; ++i) {
-        matrix.arr[i * size + (size - 1)] = linearInterpolation(i, 0.0, matrix.arr[size - 1], size - 1, matrix.arr[(size - 1) * size + (size - 1)]);
+        curmatrix.arr[i * size + (size - 1)] = linearInterpolation(i, 0.0, curmatrix.arr[size - 1], size - 1, curmatrix.arr[(size - 1) * size + (size - 1)]);
     }
 }
 
-__global__ void iterate(double* matrix, double* lastMatrix, int size) {
+__global__ void iterate(double* curmatrix, double* prevmatrix, int size) {
     int j = blockIdx.x * blockDim.x + threadIdx.x;
     int i = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (j == 0 || i == 0 || i >= size - 1 || j >= size - 1) return;
 
-    matrix[i * size + j] = 0.25 * (lastMatrix[i * size + j + 1] + lastMatrix[i * size + j - 1] +
-                                   lastMatrix[(i - 1) * size + j] + lastMatrix[(i + 1) * size + j]);
+    curmatrix[i * size + j] = 0.25 * (prevmatrix[i * size + j + 1] + prevmatrix[i * size + j - 1] +
+                                   prevmatrix[(i - 1) * size + j] + prevmatrix[(i + 1) * size + j]);
 }
 
 template <unsigned int blockSize>
-__global__ void compute_error(double* matrix, double* lastMatrix, double* max_error, int size) {
+__global__ void compute_error(double* curmatrix, double* prevmatrix, double* max_error, int size) {
     int j = blockIdx.x * blockDim.x + threadIdx.x;
     int i = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -123,7 +123,7 @@ __global__ void compute_error(double* matrix, double* lastMatrix, double* max_er
 
     if (j > 0 && i > 0 && j < size - 1 && i < size - 1) {
         int index = i * size + j;
-        double error = fabs(matrix[index] - lastMatrix[index]);
+        double error = fabs(curmatrix[index] - prevmatrix[index]);
         local_max = error;
     }
 
@@ -134,15 +134,14 @@ __global__ void compute_error(double* matrix, double* lastMatrix, double* max_er
     }
 }
 
-
-
 int main(int argc, char const *argv[]) {
     opt::options_description desc("Arguments");
     desc.add_options()
-        ("accuracy", opt::value<double>(), "Accuracy")
-        ("matr_size", opt::value<int>(), "Size of matrix")
-        ("num_iter", opt::value<int>(), "Count of iterations")
-        ("help", "Help");
+        ("accuracy", opt::value<double>()->default_value(1e-6), "accuracy")
+        ("matr_size", opt::value<int>()->default_value(256), "matrix_size")
+        ("num_iter", opt::value<int>()->default_value(1000000), "num_iterations")
+        ("help", "help");
+
     opt::variables_map vm;
     opt::store(opt::parse_command_line(argc, argv, desc), vm);
     opt::notify(vm);
@@ -150,37 +149,35 @@ int main(int argc, char const *argv[]) {
         std::cout << desc << "\n";
         return 1;
     }
-    if (!vm.count("matr_size") || !vm.count("accuracy") || !vm.count("num_iter")) {
-        std::cerr << "Missing required arguments: matr_size, accuracy, or num_iter.\n";
-        return 1;
-    }
+
     auto start = std::chrono::high_resolution_clock::now();
     int size = vm["matr_size"].as<int>();
     double accuracy = vm["accuracy"].as<double>();
     int countIter = vm["num_iter"].as<int>();
 
-    Data<double> matrix(size * size);
-    Data<double> lastMatrix(size * size);
+    Data<double> curmatrix(size * size);
+    Data<double> prevmatrix(size * size);
     
-    init(matrix, size);
-    init(lastMatrix, size);
+    init(curmatrix, size);
+    init(prevmatrix, size);
+
     double error;
     error = accuracy + 1;
     int iter = 0;
    
     dim3 blockDim(32, 32);
     dim3 gridDim((size + blockDim.x - 1) / blockDim.x, (size + blockDim.y - 1) / blockDim.y);
-    int totalBlocks = gridDim.x * gridDim.y;
+    //int totalBlocks = gridDim.x * gridDim.y;
 
     Data<double> d_max_error(gridDim.x * gridDim.y);
 
     d_max_error.copyToDevice();
-    matrix.copyToDevice();
-    lastMatrix.copyToDevice();
+    curmatrix.copyToDevice();
+    prevmatrix.copyToDevice();
 
-    double* matrix_link = matrix.getDevicePointer();
-    double* lastMatrix_link = lastMatrix.getDevicePointer();
-    double* d_max_error_link = d_max_error.getDevicePointer();
+    double* curmatrix_adr = curmatrix.getDevicePointer();
+    double* prevmatrix_adr = prevmatrix.getDevicePointer();
+    double* d_max_error_adr = d_max_error.getDevicePointer();
 
     cudaGraph_t graph;
     cudaGraphExec_t graphExec;
@@ -189,40 +186,42 @@ int main(int argc, char const *argv[]) {
 
     bool graphCreated = false;
 
-cudaMemset(d_max_error_link, 0, sizeof(double));
+    cudaMemset(d_max_error_adr, 0, sizeof(double));
 
-while (iter < countIter && error > accuracy) {
-    if (!graphCreated) {
-        cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
-        
-        for(int i = 0; i < 999; i++){
-            iterate<<<gridDim, blockDim, 0, stream>>>(matrix_link, lastMatrix_link, size);
-            std::swap(lastMatrix_link, matrix_link);
+    while (iter < countIter && error > accuracy) {
+        if (!graphCreated) {
+            cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
+            
+            for(int i = 0; i < 999; i++){
+                iterate<<<gridDim, blockDim, 0, stream>>>(curmatrix_adr, prevmatrix_adr, size);
+                std::swap(prevmatrix_adr, curmatrix_adr);
+            }
+            
+            iterate<<<gridDim, blockDim, 0, stream>>>(curmatrix_adr, prevmatrix_adr, size);
+            compute_error<32><<<gridDim, blockDim, 0, stream>>>(curmatrix_adr, prevmatrix_adr, d_max_error_adr, size);
+
+            cudaStreamEndCapture(stream, &graph);
+            cudaGraphInstantiate(&graphExec, graph, nullptr, nullptr, 0);
+            
+            graphCreated = true;
+        } else {
+            cudaGraphLaunch(graphExec, stream);
+            cudaStreamSynchronize(stream);
+            
+            double temp_error;
+            cudaMemcpy(&temp_error, d_max_error_adr, sizeof(double), cudaMemcpyDeviceToHost);
+            error = temp_error;
+
+            
+
+            std::cout << "Iteration: " << iter + 1000 << ", Error: " << error << std::endl;
+            
+            iter += 1000;
+            cudaMemset(d_max_error_adr, 0, sizeof(double));
         }
-        
-        iterate<<<gridDim, blockDim, 0, stream>>>(matrix_link, lastMatrix_link, size);
-        compute_error<32><<<gridDim, blockDim, 0, stream>>>(matrix_link, lastMatrix_link, d_max_error_link, size);
-
-        cudaStreamEndCapture(stream, &graph);
-        cudaGraphInstantiate(&graphExec, graph, nullptr, nullptr, 0);
-        
-        graphCreated = true;
-    } else {
-        cudaGraphLaunch(graphExec, stream);
-        cudaStreamSynchronize(stream);
-        
-        double temp_error;
-        cudaMemcpy(&temp_error, d_max_error_link, sizeof(double), cudaMemcpyDeviceToHost);
-        error = temp_error;
-
-        std::cout << "Iteration: " << iter + 1000 << ", Error: " << error << std::endl;
-        
-        iter += 1000;
-        cudaMemset(d_max_error_link, 0, sizeof(double));
     }
-}
 
-    matrix.copyToHost();
+    curmatrix.copyToHost();
     auto end = std::chrono::high_resolution_clock::now();
     auto time_s = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
@@ -231,13 +230,13 @@ while (iter < countIter && error > accuracy) {
     if (size <= 13) {
         for (size_t i = 0; i < size; i++) {
             for (size_t j = 0; j < size; j++) {
-                std::cout << matrix.arr[i * size + j] << ' ';
+                std::cout << curmatrix.arr[i * size + j] << ' ';
             }
             std::cout << std::endl;
         }
     }
 
-    write_matrix(matrix.arr.data(), size, "matrix2.txt");
+    write_matrix(curmatrix.arr.data(), size, "matrix2.txt");
 
     cudaStreamDestroy(stream);
     cudaGraphExecDestroy(graphExec);
